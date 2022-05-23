c798f65c475461b3cc98a93be0532b0bb9b73c69
Guillermo Malaga <gmg.proyectos.2013@gmail.com>
2022-05-22 22:15:41 -0400

    evaluacion3 terminada

--- EjercicioJS.js
+++ EjercicioJS.js
@@ -1,4 +1,4 @@
-//arreglo de coleres
+//arreglo de colores
 var colors = document.querySelectorAll(".square");
 var colorUnico= document.querySelector(".colorDisplay");
 var tituloH1=document.querySelector("h1");
@@ -6,14 +6,52 @@ var colorFondo="RGB(35, 35, 35)";
 var boton=document.querySelector("#boton");
 var colorEscogido="";
 
-console.log("esto en un aproba");
+// var  padreDiv=colors[0].parentNode;
+// colors[3]= padreDiv.removeChild(colors[3]);
+// colors[4]= padreDiv.removeChild(colors[4]);
+// colors[5]= padreDiv.removeChild(colors[5]);
+
+console.log("esto en una prueba");
 console.log(colors);
+
 //funcion para generar numeros  aleatorios
 function generarColor(num){
     return Math.floor(Math.random() * (num));
 }
+
+//funcion para colocae los cuadros segun sea facil o dificil
+var facil=document.querySelector("#facil");
+var dificil=document.querySelector("#dificil");
+
+dificil.addEventListener("click",function(){
+    if(document.querySelectorAll(".square").length>3){
+        console.log("El numero de cuadros es mayor a 3");
+        let padre=colors[0].parentNode;
+        colors[3]=padre.removeChild(colors[3]);
+        colors[4]=padre.removeChild(colors[4]);
+        colors[5]=padre.removeChild(colors[5]);      
+        Inicio();  
+    }else{
+        console.log("El numero de cuadros es igual a 3");
+    }
+});
+
+facil.addEventListener("click",function(){
+    if(document.querySelectorAll(".square").length>3){
+        console.log("El numero de cuadros es mayor a 3");
+    }else{
+        console.log("El numero de cuadros es igual a 3");
+        let padre=colors[0].parentNode;
+        padre.appendChild(colors[3]);
+        padre.appendChild(colors[4]);
+        padre.appendChild(colors[5]);        
+        Inicio();
+    }
+});
+
 function Inicio(){
-    for (let i = 0; i < 6; i++) {
+    let colors = document.querySelectorAll(".square");
+    for (let i = 0; i < colors.length; i++) {
         let colorCuadro="RGB("+generarColor(255)+", "+generarColor(255)+", "+generarColor(255)+")";
         colors[i].style.backgroundColor=colorCuadro;
         console.log(colors[i].style.backgroundColor);  
@@ -23,19 +61,18 @@ function Inicio(){
                 console.log("el color es: "+this.style.backgroundColor);
                 for (let j = 0; j < colors.length; j++) {
                     colors[j].style.backgroundColor=colorEscogido;
-                    tituloH1.style.backgroundColor=colorEscogido;
-                    
+                    tituloH1.style.backgroundColor=colorEscogido;                    
                 }
             }else{
                 //aqui se ejecuta codigo en el caso de fallar se pone el div color del fondo
                 this.style.backgroundColor=colorFondo;
-            }
-            
+            }            
         });  
     }
-    colorEscogido=colors[generarColor(5)].style.backgroundColor;
+    colorEscogido=colors[generarColor(colors.length)].style.backgroundColor;
     colorUnico.innerHTML=colorEscogido;
 }
+
 Inicio();
 boton.addEventListener("click",function(){
     Inicio();
